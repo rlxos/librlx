bab7198b4b07fe7c1011a9ddd012a2031f00362b
itsmanjeet <itsmanjeet1998@gmail.com>
2021-06-15 17:35:16 +0530

    [Auto Sync]: A lot to do individual

--- path/path.hh
+++ path/path.hh
@@ -3,18 +3,34 @@
 
 #include <string>
 #include <fstream>
+#include <libgen.h>
+
+#include "../io.hh"
 
 namespace rlx::path
 {
-    inline bool has_ext(std::string const& p, std::string const& ext)
+    inline bool has_ext(std::string const &p, std::string const &ext)
     {
-        if (p.length() == 0)
+        if (p.length() == 0 || p.length() < ext.length())
             return false;
-        return p.rfind(ext, 0) == 0;
+
+        rlx::io::debug(rlx::io::debug_level::trace, "CHECK EXT : ", p.substr(ext.length(), p.length() - ext.length()), " == ", ext);
+
+        return p.substr(p.length() - ext.length(), ext.length()) == ext;
     }
 
-    template<typename T>
-    inline void writefile(std::string const& p, T const& content)
+    inline std::string basename(std::string path)
+    {
+        return ::basename((char *)path.c_str());
+    }
+
+    inline std::string dirname(std::string path)
+    {
+        return ::dirname((char *)path.c_str());
+    }
+
+    template <typename T>
+    inline void writefile(std::string const &p, T const &content)
     {
         std::ofstream file(p);
         file << content;
